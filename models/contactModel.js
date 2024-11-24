4a0b04e579fc7932d18d402864802fa8243a0e81
Prince0906 <princesahoo659@gmail.com>
2024-11-24 12:31:26 +0530

    added all required models

--- models/contactModel.js
+++ models/contactModel.js
@@ -6,41 +6,98 @@ const createTableQuery = `
     phoneNumber varchar(255),
     email varchar(255),
     linkedId int,
-    linkPrecedence varchar(50) NOT NULL,
-    createdAt timestamp DEFAULT current_timestamp,
-    updatedAt timestamp DEFAULT current_timestamp ON UPDATE current_timestamp,
-    deletedAt timestamp NULL,
-    FOREIGN KEY (linkedId) REFERENCES contact(id)
+    linkPrecedence enum('primary', 'secondary') not null default 'primary',
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp,
+    deletedAt timestamp null,
+    foreign key (linkedId) references contact(id)
   );
 `;
 
 const initializeDatabase = async () => {
     try {
-        const connection = await pool.getConnection();
-        await connection.query(createTableQuery);
-        console.log("Table 'contact' ensured to exist.");
-        connection.release();
+      const connection = await pool.getConnection();
+      await connection.query(createTableQuery);
+      console.log("Table 'contact' ensured to exist.");
+      connection.release();
     } catch (error) {
-        console.error("Error initializing the database:", error.message);
+      console.error("Error initializing the database:", error.message);
     }
 };
-
-
-const getAllContacts = async () => {
-    const query = "SELECT * FROM contact WHERE deletedAt IS NULL";
-    return pool.execute(query);
-};
-
-const createContact = async (data) => {
-    const { phoneNumber, email, linkedId, linkPrecedence } = data;
+  
+  const executeQuery = async (query, params = []) => {
+    const connection = await pool.getConnection();
+    try {
+      const [results] = await connection.query(query, params);
+      return results;
+    } finally {
+      connection.release();
+    }
+  };
+  
+  const addContact = async (contact) => {
     const query = `
-        INSERT intO contact (phoneNumber, email, linkedId, linkPrecedence)
-        VALUES (?, ?, ?, ?)
+      insert into contact (email, phoneNumber, linkedId, linkPrecedence, createdAt, updatedAt)
+      values (?, ?, ?, ?, now(), now());
     `;
-    return pool.execute(query, [phoneNumber, email, linkedId, linkPrecedence]);
-};
-module.exports = {
+    const linkPrecedence = contact.linkedId ? 'secondary' : 'primary';
+    await executeQuery(query, [
+      contact.email,
+      contact.phoneNumber,
+      contact.linkedId,
+      linkPrecedence,
+    ]);
+  };
+  
+  const mergePrimaries = async (primaryId, secondaryId) => {
+    const query = `
+      update contact set linkedId = ?, linkPrecedence = 'secondary', updatedAt = now()
+      where linkedId = ? or id = ?;`;
+    await executeQuery(query, [primaryId, secondaryId, secondaryId]);
+  };
+  
+  const getPrimaryViaEmail = async (email) => {
+    const query1 = `select * from contact where email = ? limit 1;`;
+    const anyEmailLinked = (await executeQuery(query1, [email]))[0];
+    if (!anyEmailLinked) return null;
+    if (!anyEmailLinked.linkedId) return anyEmailLinked;
+  
+    const query2 = `select * from contact where id = ? limit 1;`;
+    return (await executeQuery(query2, [anyEmailLinked.linkedId]))[0] || null;
+  };
+  
+  const getPrimaryViaPhoneNumber = async (phoneNumber) => {
+    const query1 = `select * from contact where phoneNumber = ? limit 1;`;
+    const anyPhoneLinked = (await executeQuery(query1, [phoneNumber]))[0];
+    if (!anyPhoneLinked) return null;
+  
+    if (!anyPhoneLinked.linkedId) return anyPhoneLinked;
+  
+    const query2 = `select * from contact where id = ? limit 1;`;
+    return (await executeQuery(query2, [anyPhoneLinked.linkedId]))[0] || null;
+  };
+  
+  const getLinkedContacts = async (contactLike) => {
+    const query1 = `select * from contact where email = ? or phoneNumber = ? limit 1;`;
+    const anyLinked = (
+      await executeQuery(query1, 
+        [contactLike.email || "NO MATCH", 
+        contactLike.phoneNumber || "NO MATCH"])
+    )[0];
+  
+    if (!anyLinked) return [];
+  
+    const primaryId = anyLinked.linkedId || anyLinked.id;
+  
+    const query2 = `select * from contact where linkedId = ? or id = ? order by createdAt asc;`;
+    return await executeQuery(query2, [primaryId, primaryId]);
+  };
+  
+  module.exports = {
     initializeDatabase,
-    getAllContacts,
-    createContact,
-};
+    addContact,
+    mergePrimaries,
+    getPrimaryViaEmail,
+    getPrimaryViaPhoneNumber,
+    getLinkedContacts,
+  };
